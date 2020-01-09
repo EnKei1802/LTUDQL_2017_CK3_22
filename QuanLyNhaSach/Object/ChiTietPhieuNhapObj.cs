06d480ef5d281646c95198ff973cdc12f0b58736
EnKei1802 <nguyenkhuongdp1@gmail.com>
2020-01-09 12:19:13 +0700

    Dựng các form còn lại

---
+++ QuanLyNhaSach/Object/ChiTietPhieuNhapObj.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhaSach.Object
+{
+    class ChiTietPhieuNhapObj
+    {
+        //code o day
+    }
+}
---
+++ QuanLyNhaSach/Object/PhieuThuTienObj.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhaSach.Object
+{
+    class PhieuThuTienObj
+    {
+        //code o day
+        private string maPhieuThuTien;
+        private string maKhachHang;
+        private DateTime ngayThuTien;
+        private float soTienThu;
+
+        public string MaPhieuThuTien { get => maPhieuThuTien; set => maPhieuThuTien = value; }
+        public string MaKhachHang { get => maKhachHang; set => maKhachHang = value; }
+        public DateTime NgayThuTien { get => ngayThuTien; set => ngayThuTien = value; }
+        public float SoTienThu { get => soTienThu; set => soTienThu = value; }
+    }
+}
