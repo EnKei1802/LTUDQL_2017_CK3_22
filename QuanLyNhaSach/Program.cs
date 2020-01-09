ccc2a55995a0e29df2926c14e85fede065eb8a4d
EnKei1802 <nguyenkhuongdp1@gmail.com>
2020-01-09 12:09:26 +0700

    Bổ sung commited

--- QuanLyNhaSach/Program.cs
+++ QuanLyNhaSach/Program.cs
@@ -17,7 +17,7 @@ namespace QuanLyNhaSach
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmKhachHang());
+            Application.Run(new frmSach());
         }
     }
 }
