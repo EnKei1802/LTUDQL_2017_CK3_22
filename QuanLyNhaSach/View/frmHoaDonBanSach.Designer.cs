796b36565ce5c58384620a67b2daa35f10af09cc
EnKei1802 <nguyenkhuongdp1@gmail.com>
2020-01-09 14:51:30 +0700

    Chỉnh sửa form Hóa đơn

--- QuanLyNhaSach/View/frmHoaDonBanSach.Designer.cs
+++ QuanLyNhaSach/View/frmHoaDonBanSach.Designer.cs
@@ -237,7 +237,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(56, 119);
+            this.label1.Location = new System.Drawing.Point(56, 40);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(128, 17);
             this.label1.TabIndex = 11;
@@ -246,16 +246,16 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(56, 33);
+            this.label2.Location = new System.Drawing.Point(56, 126);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.Size = new System.Drawing.Size(40, 17);
             this.label2.TabIndex = 12;
-            this.label2.Text = "Mã sách";
+            this.label2.Text = "Sách";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(56, 76);
+            this.label3.Location = new System.Drawing.Point(56, 83);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(83, 17);
             this.label3.TabIndex = 13;
@@ -407,6 +407,7 @@
             this.Controls.Add(this.groupBox1);
             this.Name = "frmHoaDonBanSach";
             this.Text = "frmHoaDonBanSach";
+            this.Load += new System.EventHandler(this.frmHoaDonBanSach_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
